4ce1b3d78dd6e561d8af5909e6d71d16663dfef3
zfy <470981891@qq.com>
2025-10-21 11:15:45 +0800

    feat: react19兼容antd

--- frontend/apps/web/src/main.tsx
+++ frontend/apps/web/src/main.tsx
@@ -2,6 +2,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { CopilotKit } from '@copilotkit/react-core';
+import '@ant-design/v5-patch-for-react-19';
 
 import './index.scss';
 
