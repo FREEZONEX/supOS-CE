0a0d8577096fa71dfa7990c0d970dc624b59a132
zfy <470981891@qq.com>
2025-10-30 16:58:16 +0800

    fix: 修复health接口

--- frontend/apps/services-express/src/config/index.ts
+++ frontend/apps/services-express/src/config/index.ts
@@ -9,8 +9,8 @@ export const config = {
   port: parseInt(process.env.PORT || '4000', 10),
   nodeEnv: process.env.NODE_ENV || 'development',
   // Docker配置
-  dockerHost: process.env.DOCKER_HOST || 'localhost',
-  dockerPort: parseInt(process.env.DOCKER_PORT || '2375', 10),
+  dockerHost: process.env.DOCKER_HOST || 'host.docker.internal',
+  dockerPort: parseInt(process.env.DOCKER_PORT || '2376', 10),
   llmType: process.env.LLM_TYPE || 'openai',
   ollamaModal: process.env.LLM_MODEL || 'llama3.2',
   ollamaBaseurl: process.env.LLM_BASEURL || 'http://office.unibutton.com:11435',
