60954dbeb0eb5ebf78a4a1a8ed596d37ac8dc3e2
houkangxi <houkangxi@supos.com>
2025-12-11 16:29:19 +0800

    fix: 导出优化格式为数组；feat:导出类型改名

--- backend/internal/repo/relationDB/unsNamespace_export.go
+++ backend/internal/repo/relationDB/unsNamespace_export.go
@@ -14,7 +14,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-var selectColumns = []string{"id", "parent_id", "model_id", "alias", "name", "display_name",
+var selectColumns = []string{"id", "path_type", "parent_id", "model_id", "alias", "name", "display_name",
 	"expression", "description", "label_ids",
 	"protocol", "refers", "data_type", "parent_data_type", "with_flags", "fields"}
 
@@ -52,16 +52,9 @@ func (p UnsNamespaceRepo) ExportCsvByIds(ids []int64, w io.Writer) error {
 	_, err = conn.Conn().PgConn().CopyTo(context.Background(), w, query)
 	return err
 }
-func (p UnsNamespaceRepo) ExportCsvByFolderIds(folderIds []int64, w io.Writer) error {
-	dbPool := getDbPool()
-	conn, err := dbPool.Acquire(context.Background())
-	if err != nil {
-		return err
-	}
-	defer conn.Release()
-
+func (p UnsNamespaceRepo) ExportCsvByLayRecAndIds(layRec []string, ids []int64, w io.Writer) error {
 	query := base.StringBuilder{}
-	query.Grow(64 + 32*len(folderIds))
+	query.Grow(64 + 64*len(layRec) + 20*len(ids))
 	query.Append("COPY (SELECT ")
 	for i, col := range selectColumns {
 		if i > 0 {
@@ -69,15 +62,40 @@ func (p UnsNamespaceRepo) ExportCsvByFolderIds(folderIds []int64, w io.Writer) e
 		}
 		query.Append(col)
 	}
-	query.Append(" FROM ").Append(TableNameUnsNamespace).Append(" WHERE (")
-	for i, id := range folderIds {
-		if i > 0 {
-			query.Append(" OR ")
+	query.Append(" FROM ").Append(TableNameUnsNamespace).Append(" WHERE ")
+	if len(layRec) > 0 || len(ids) > 0 {
+		query.Append(" ( ")
+		if len(layRec) > 0 {
+			query.Append(" ( ")
+			for i, lay := range layRec {
+				if i > 0 {
+					query.Append(" OR ")
+				}
+				query.Append("lay_rec like '").Append(lay).Append("%'")
+			}
+			query.Append(")")
 		}
-		query.Append("lay_rec like '").Long(id).Append("%'")
+		if len(ids) > 0 {
+			if len(layRec) > 0 {
+				query.Append(" OR ")
+			}
+			query.Append(" id IN (")
+			for _, id := range ids {
+				query.Long(id).Append(",")
+			}
+			query.SetLast(')')
+		}
+		query.Append(" ) AND ")
 	}
-	query.Append(") and status=1 and id>1000  and (data_type is null OR data_type<>5 ) order by lay_rec asc)").
+	query.Append(" status=1 and id>1000  and (data_type is null OR data_type<>5 ) order by lay_rec asc)").
 		Append("TO STDOUT WITH CSV HEADER")
+
+	dbPool := getDbPool()
+	conn, err := dbPool.Acquire(context.Background())
+	if err != nil {
+		return err
+	}
+	defer conn.Release()
 	_, err = conn.Conn().PgConn().CopyTo(context.Background(), w, query.String())
 	return err
 }
@@ -114,6 +132,12 @@ func init() {
 	exportFunctions["id"] = func(po *UnsNamespace, val string) {
 		id, _ := strconv.ParseInt(val, 10, 64)
 		po.Id = id
+	}
+	exportFunctions["path_type"] = func(po *UnsNamespace, val string) {
+		Int, er := strconv.Atoi(val)
+		if er == nil {
+			po.PathType = int16(Int)
+		}
 	}
 	exportFunctions["parent_id"] = func(po *UnsNamespace, val string) {
 		pid, er := strconv.ParseInt(val, 10, 64)
