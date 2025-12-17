863dc4b7922397ca3aa2ad6623832a1bde2b5eee
zhuangmh <15167160211@163.com>
2025-12-17 14:41:49 +0800

    fix: 添加清理数据变量

--- deploy/bin/util/clean-images.sh
+++ deploy/bin/util/clean-images.sh
@@ -4,7 +4,7 @@ set -e
 CLEAN_IMAGES=$1
 
 if [ -n "$CLEAN_IMAGES" ]; then
-    docker rmi -f $(docker images | grep $CLEAN_IMAGES | awk '{print $1 ":" $2}')
+    docker rmi -f $(docker images | grep "$CLEAN_IMAGES" | awk '{print $1 ":" $2}')
 else 
     docker rmi -f $(docker images | awk '{print $1 ":" $2}')
 fi
