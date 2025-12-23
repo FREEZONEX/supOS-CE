a528f6f43e5b533ba5687f2d515a61f0b0c963a0
houkangxi <houkangxi@supos.com>
2025-12-23 13:42:42 +0800

    feat: +导入flags处理

--- backend/internal/logic/supos/uns/importExport/service/FileData.go
+++ backend/internal/logic/supos/uns/importExport/service/FileData.go
@@ -12,6 +12,7 @@ import (
 	"backend/share/spring"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 )
@@ -104,7 +105,7 @@ func node2vo(prop string, i, parent *FileData) *types.CreateTopicDto {
 		switch i.Type {
 		case TYPE_FILE, "file":
 			vo.PathType = constants.PathTypeFile
-			if os.Getenv("SYS_OS_ENABLE_AUTO_CATEGORIZATION") == "true" {
+			if ok, _ := strconv.ParseBool(os.Getenv("SYS_OS_ENABLE_AUTO_CATEGORIZATION")); ok {
 				if i.ParentDataType == "" {
 					i.Error = I18nUtils.GetMessage("uns.excel.parentDataType.is.blank")
 					return nil
@@ -147,7 +148,10 @@ func node2vo(prop string, i, parent *FileData) *types.CreateTopicDto {
 		vo.Description = &i.Description
 	}
 	if len(i.Label) > 0 {
-		vo.LabelNames = strings.Split(i.Label, ",")
+		vo.LabelNames = base.FilterAndMap(strings.Split(i.Label, ","), func(s string) (string, bool) {
+			s = strings.TrimSpace(s)
+			return s, len(s) > 0
+		})
 	}
 	if len(i.DataType) > 0 {
 		dt := enums.DataTypeInt(i.DataType)
@@ -169,7 +173,28 @@ func node2vo(prop string, i, parent *FileData) *types.CreateTopicDto {
 			}
 		}
 	}
+	if vo.PathType == constants.PathTypeFile {
+		vo.AddDashBoard = parseBoolP(i.GenerateDashboard)
+		vo.Save2Db = parseBoolP(i.EnableHistory)
+
+		if base.P2v(vo.DataType) == constants.JsonbType {
+			vo.AddFlow = base.OptionalFalse
+		} else {
+			vo.AddFlow = parseBoolP(i.MockData)
+		}
+	}
 	return vo
+}
+func parseBoolP(str string) *bool {
+	str = strings.TrimSpace(str)
+	if str == "" {
+		return nil
+	}
+	if v, er := strconv.ParseBool(str); er == nil {
+		return &v
+	} else {
+		return base.OptionalFalse
+	}
 }
 
 var initDefOnce sync.Once
