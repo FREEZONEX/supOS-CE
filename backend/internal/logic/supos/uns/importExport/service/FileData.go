b10992ed0f665815bdcad2123ba2bcb4ab458bac
houkangxi <houkangxi@supos.com>
2026-01-12 11:06:06 +0800

    fix#101307#101299 导入进度超过100%；类型强限制

--- backend/internal/logic/supos/uns/importExport/service/FileData.go
+++ backend/internal/logic/supos/uns/importExport/service/FileData.go
@@ -102,7 +102,7 @@ func node2vo(prop string, i, parent *FileData) *types.CreateTopicDto {
 		vo.PathType = constants.PathTypeTemplate
 	case UNS:
 		switch i.Type {
-		case TYPE_FILE, "file":
+		case TYPE_FILE:
 			vo.PathType = constants.PathTypeFile
 			if ok, _ := strconv.ParseBool(os.Getenv("SYS_OS_ENABLE_AUTO_CATEGORIZATION")); ok {
 				if i.ParentDataType == "" {
@@ -110,7 +110,7 @@ func node2vo(prop string, i, parent *FileData) *types.CreateTopicDto {
 					return nil
 				}
 			}
-		case TYPE_FOLDER, "folder":
+		case TYPE_FOLDER:
 			vo.PathType = constants.PathTypeDir
 			if i.Name == "label" || i.Name == "template" {
 				i.Error = I18nUtils.GetMessage("uns.folder.reserved.word")
