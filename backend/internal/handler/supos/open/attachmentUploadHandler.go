a6ca28d9e40da89015ac413a076ee8b2fa8e9e95
xinwangji <xinwangji@supos.com>
2026-02-04 13:18:11 +0800

    feat: 附件上传

--- backend/internal/handler/supos/open/attachmentUploadHandler.go
+++ backend/internal/handler/supos/open/attachmentUploadHandler.go
@@ -9,15 +9,26 @@ import (
 	"backend/internal/logic/supos/open"
 	"backend/internal/svc"
 	"backend/internal/types"
+
+	"gitee.com/unitedrhino/share/errors"
+	"gitee.com/unitedrhino/share/result"
 	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 // 模板实例附件上传
 func AttachmentUploadHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		const maxBodySize = 10 << 20
+		r.Body = http.MaxBytesReader(w, r.Body, maxBodySize)
+
+		if err := r.ParseMultipartForm(maxBodySize); err != nil { // 10MB max memory
+			httpx.ErrorCtx(r.Context(), w, errors.Parameter.WithMsg("uns.importDocumentMax"))
+			return
+		}
+
 		var req types.AttachmentUploadReq
 		if err := httpx.Parse(r, &req); err != nil {
-			httpx.ErrorCtx(r.Context(), w, err)
+			result.Http(w, r, nil, errors.Parameter.WithMsg("入参不正确:"+err.Error()))
 			return
 		}
 
@@ -26,7 +37,7 @@ func AttachmentUploadHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 		if err != nil {
 			httpx.ErrorCtx(r.Context(), w, err)
 		} else {
-			httpx.OkJsonCtx(r.Context(), w, resp)
+			result.Http(w, r, resp, err)
 		}
 	}
 }
