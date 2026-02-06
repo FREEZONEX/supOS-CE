0a1172cb0aa431d3559e1eca3d26c76bb798f39c
xinwangji <xinwangji@supos.com>
2026-02-06 09:56:57 +0800

    feat: sql 多了个,

--- backend/internal/repo/relationDB/migrations_sqls/nodeRed.sql
+++ backend/internal/repo/relationDB/migrations_sqls/nodeRed.sql
@@ -25,7 +25,6 @@ CREATE TABLE if not exists supos_node_flow_top_recodes (
                                                            user_id varchar Not NULL,
                                                            mark int2 default 1,
                                                            mark_time timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
-                                                           update_time timestamptz NULL,
-);
+                                                           update_time timestamptz NULL);
 CREATE UNIQUE INDEX idx_unique_id_user ON supos_node_flow_top_recodes (id, user_id);
 alter table supos_node_flows add if not exists "creator" varchar(128) NULL;
