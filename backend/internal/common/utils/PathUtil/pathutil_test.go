12e3269aa2eca4688ea3f8c641adc5aede24cd2d
houkangxi <houkangxi@supos.com>
2026-03-03 09:30:57 +0800

    fix#102440 别名正则不对

--- backend/internal/common/utils/PathUtil/pathutil_test.go
+++ backend/internal/common/utils/PathUtil/pathutil_test.go
@@ -3,7 +3,7 @@ package PathUtil
 import "testing"
 
 func TestIsAliasFormatOK(t *testing.T) {
-	alias := []string{"123", "bug@34%$ASDFsd@#中文", "Normal123"}
+	alias := []string{"123", "4AIgc", "bug@34%$ASDFsd@#中文", "Normal123", "aigc"}
 	for _, v := range alias {
 		t.Log(v, " Ok ? ", IsAliasFormatOK(v))
 	}
