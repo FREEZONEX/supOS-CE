aa4eb233bf2bdd133781b5ac661e0cc3cd37c9e8
zhuangmh <zhuangmh@supos.com>
2026-03-11 14:32:48 +0800

    还原部分被覆盖的文件

--- deploy/mount/node-red/offline_modules/node-supmodel-emqx/modbus-bridge.js
+++ deploy/mount/node-red/offline_modules/node-supmodel-emqx/modbus-bridge.js
@@ -19,7 +19,7 @@ class ModbusBridge {
             while ((newMsg = this.queue.poll()) != null) {
                 node.send([newMsg])
             }
-        }, interval); // 轮询队列里的数据，每次只取一个topic发送
+        }, interval); // 轮询队列里的数据
         node.log(`MODBUS ${interval}ms定时推送任务开启, 节点ID=${node.id}`)
     }
 
